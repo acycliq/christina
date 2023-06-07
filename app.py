7e69e1d024dbb90adacd376fc9e112f5f26adeb3
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-06-07 13:50:12 +0100

    added microglia

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import diplib as dip
 import os
 import logging
 
-def normalize_img(img, normalize=[0,100]):
+def normalize_img(img, normalize=[0,99.9]):
     perc1, perc2 = np.percentile(img, list(normalize))
     img = img - perc1
     img /= (perc2-perc1)
@@ -17,13 +17,15 @@ def normalize_img(img, normalize=[0,100]):
     return img
 
 
-def stack_to_images(filename):
+def stack_to_images(d):
+    label = d[0]
+    filename = d[1]
     dapi = skimage.io.imread(filename)
     n = dapi.shape[0]
     for i in range(n):
         page = normalize_img(dapi[i])
         img = Image.fromarray(page.astype(np.uint8))
-        target_file = os.path.join(os.path.join('pages', 'page_%s.jpg' % str(i).zfill(3)))
+        target_file = os.path.join(os.path.join('pages', label, 'page_%s.jpg' % str(i).zfill(3)))
         if not os.path.exists(os.path.dirname(target_file)):
             os.makedirs(os.path.dirname(target_file))
         img.save(target_file)
@@ -31,7 +33,7 @@ def stack_to_images(filename):
 
         scale = 0.5
         img_resized = img.resize([int(scale * s) for s in img.size], Image.ANTIALIAS)
-        target_file = os.path.join(os.path.join('pages', 'resized_page_%s.jpg' % str(i).zfill(3)))
+        target_file = os.path.join(os.path.join('pages', label, 'resized_page_%s.jpg' % str(i).zfill(3)))
         img_resized.save(target_file)
         print('resized_page_%d.jpg saved at %s' % (i, target_file))
 
@@ -41,8 +43,13 @@ def app():
 
 
 if __name__ == "__main__":
-    img_path = r"F:\data\Christina\microglia\DAPI_retiled_image.tif"
-    stack_to_images(img_path)
+    dic = {
+        # 'dapi': r"F:\data\Christina\microglia\DAPI_retiled_image.tif",
+        'microglia': r"F:\data\Christina\microglia\microglia_ WT997_icvAB_Iba1_retiled.tif"
+    }
+    for di in dic.items():
+        # img_path = r"F:\data\Christina\microglia\DAPI_retiled_image.tif"
+        stack_to_images(di)
     # img = skimage.io.imread(img_path)
     # nimg = normalize_img(img)
     # im = Image.fromarray(nimg)
